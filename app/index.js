0a2ab08d338492b4e4e133275699ec02cc20f15a
Waqar Akram <waqarakrambhutta@gmail.com>
2024-08-20 03:37:02 +0500

    App template is ready

---
+++ app/index.js
@@ -0,0 +1,42 @@
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./src/screens/HomeScreen";
+import ChatScreen from "./src/screens/ChatScreen";
+import comp from "./src/screens/comp";
+
+const Stack = createStackNavigator();
+export default function Page() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="chatScreen" component={ChatScreen} />
+      <Stack.Screen name="comp" component={comp} />
+    </Stack.Navigator>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 24,
+  },
+  main: {
+    flex: 1,
+    justifyContent: "center",
+    maxWidth: 960,
+    marginHorizontal: "auto",
+  },
+  title: {
+    fontSize: 64,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 36,
+    color: "#38434D",
+  },
+});
---
+++ app/src/screens/HomeScreen.js
@@ -0,0 +1,67 @@
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import React from "react";
+
+const List = [
+  {
+    id: 1,
+    name: "Chat Screen",
+    navigateTO: "chatScreen",
+  },
+  {
+    id: 2,
+    name: "comp",
+    navigateTO: "comp",
+  },
+  {
+    id: 3,
+    name: "Chat Screen",
+    navigateTO: "chatScreen",
+  },
+  {
+    id: 4,
+    name: "Chat Screen",
+    navigateTO: "chatScreen",
+  },
+];
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={List}
+        key={(item) => {
+          item.id;
+        }}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate(item.navigateTO);
+            }}
+          >
+            <Text style={styles.text}>
+              {item.id}- {item.name}
+            </Text>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 20,
+  },
+  text: {
+    color: "blue",
+    fontSize: 18,
+    textDecorationLine: "underline",
+  },
+});
+
+export default HomeScreen;
